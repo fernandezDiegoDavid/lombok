1ef3db100f29fcc0eb4ab87662f76d5e60979ee1
Fernandez Diego <diegodavid712@gmail.com>
2023-05-31 01:41:24 -0300

    primer practica de lombok

---
+++ src/main/java/com/example/demo/lombok/Main.java
@@ -0,0 +1,22 @@
+package com.example.demo.lombok;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona per = new Persona();
+		per.setEdad(40);
+		per.setApellido("fernandez");
+		per.setNombre("David");
+		System.out.println("edad: "+per.getEdad());
+		Persona per2 = new Persona("Diego", 31,"Fernandez");
+		System.out.println(per2);
+		System.out.println("comparacion de objetos");
+		System.out.println(per2.equals(per));
+		Persona pers3= new Persona("Diego",31,"Fernandez");
+		System.out.println(per2.equals(pers3));
+		
+
+	}
+
+}
---
+++ src/main/java/com/example/demo/lombok/Persona.java
@@ -0,0 +1,21 @@
+package com.example.demo.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+
+	private String nombre;
+	private int edad;
+	private String apellido;
+}
